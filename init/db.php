fb8160d0dce8c1abde652116cbf33fc2f715376c
Chisae <chisae0123@gmail.com>
2025-02-04 19:26:27 +0700

    week2

---
+++ init/db.php
@@ -0,0 +1,14 @@
+<?php
+    $host = 'localhost'; // '127.0.0.1'
+    $db_name = 'webproject'; // semi colon
+    $user = 'root';
+    $passwd = '';
+
+
+    $db = new mysqli($host,$user,$passwd,$db_name);
+    if($db->connect_error){
+        echo 'connection failed';
+        die();
+    } 
+
+?>
