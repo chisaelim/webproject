f389a78cc3fc7a2eb312cad16652f00e7aea2e09
Chisae <chisae0123@gmail.com>
2025-03-04 22:25:56 +0700

    week 5

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/webproject/">Navbar</a>
+  <div class="container">
+    <a class="navbar-brand" href="<?php echo loggedInUser() ? '/webproject/pages/dashboard.php' : '/webproject'  ?>">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <!-- <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li> -->
@@ -17,17 +17,43 @@
             Account
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/webproject/pages/login.php">Login</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="/webproject/pages/register.php">Register</a></li>
+
+            <?php
+            // if (!loggedInUser()) {
+            //   echo ('<li><a class="dropdown-item" href="/webproject/pages/login.php">Login</a></li>
+            //   <li>
+            //     <hr class="dropdown-divider">
+            //   </li>
+            //   <li><a class="dropdown-item" href="/webproject/pages/register.php">Register</a></li>');
+            // } else {
+            //   echo ('<li><a class="dropdown-item" href="/webproject/pages/logout.php">Logout</a></li>');
+            // }
+            ?>
+
+
+            <?php
+            if (!loggedInUser()) {
+            ?>
+
+              <li><a class="dropdown-item" href="/webproject/pages/login.php">Login</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="/webproject/pages/register.php">Register</a></li>
+
+            <?php
+            } else {
+            ?>
+
+              <li><a class="dropdown-item" href="/webproject/pages/logout.php">Logout</a></li>
+
+            <?php
+            }
+            ?>
+
+
             <!-- <li><a class="dropdown-item" href="#">Something else here</a></li> -->
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/webproject/pages/logout.php">Logout</a>
-
         </li>
       </ul>
       <!-- <form class="d-flex" role="search">
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 include("./init/db.php");
+
+require_once('./init/func.php');
+
 include("./includes/header.php");
 include("./includes/navbar.php");
+
+$x = 1;
+echo '$x<h5>home page</h5>'; 
 ?>
 
 <h1>home page</h1>
